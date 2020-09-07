43807f98e579a443a785917f48fd3146cbe113df
AmirMohammad Pirzadeh <am.pirzadeh@gmail.com>
2020-09-07 19:53:24 +0430

    Initial dist subtree commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# Movie Searcher
-
-This is a movie searcher that uses OMDb API to get information about movies and series and display them, built with VueJs.
---
+++ dist/precache-manifest.063d9063d6f43dab63b48c20752daea6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8179482a9ef11cee2094",
+    "url": "/Movie-Search/css/chunk-0c823712.21a14106.css"
+  },
+  {
+    "revision": "3367c95fdba5827fc727",
+    "url": "/Movie-Search/css/chunk-2e8bb381.c309eb75.css"
+  },
+  {
+    "revision": "63e55379ef217db507e2",
+    "url": "/Movie-Search/css/chunk-32a98358.e633602d.css"
+  },
+  {
+    "revision": "3d7eb8ecfe3003fe08f7",
+    "url": "/Movie-Search/css/chunk-vendors.55712569.css"
+  },
+  {
+    "revision": "59b2ad213267c2991d1303e2d1df9208",
+    "url": "/Movie-Search/index.html"
+  },
+  {
+    "revision": "9f2fbef882947b694c81",
+    "url": "/Movie-Search/js/app.a4241940.js"
+  },
+  {
+    "revision": "8179482a9ef11cee2094",
+    "url": "/Movie-Search/js/chunk-0c823712.9cf399f6.js"
+  },
+  {
+    "revision": "3367c95fdba5827fc727",
+    "url": "/Movie-Search/js/chunk-2e8bb381.64f032c4.js"
+  },
+  {
+    "revision": "63e55379ef217db507e2",
+    "url": "/Movie-Search/js/chunk-32a98358.0398313f.js"
+  },
+  {
+    "revision": "3d7eb8ecfe3003fe08f7",
+    "url": "/Movie-Search/js/chunk-vendors.30d7d548.js"
+  },
+  {
+    "revision": "762070e0acbd4143d103d1b27d363b70",
+    "url": "/Movie-Search/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/Movie-Search/robots.txt"
+  }
+]);
