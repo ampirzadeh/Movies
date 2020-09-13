6b40786e3671254445dc4c6537899611c1fec41e
ampirzadeh <am.pirzadeh@gmail.com>
2020-09-13 12:58:40 +0000

    Deploying to gh-pages from  @ 4fb99751557bf0e303414fc8033c26641107e0a9 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,16 +14,12 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Movies/precache-manifest.9abe413d5f94be9f5d7dd989ff459d2d.js"
+  "/movie-search/precache-manifest.3c1e4783a0823452ca9e23fc674d4e49.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "movie-search"});
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+workbox.core.skipWaiting();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
